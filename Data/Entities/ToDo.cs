35b1634830e698f64b2f1f1a16126cb567066435
Ramiro Di Carlo <ramirodicarlo2@gmail.com>
2023-11-29 16:15:01 -0300

    FixiesContext

--- Data/Entities/ToDo.cs
+++ Data/Entities/ToDo.cs
@@ -11,7 +11,6 @@ namespace TODOLIST.Data.Entities
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        public int RealtedProject { get; set; }
         [ForeignKey("UserId")]
         public Programer Programer { get; set; }
         public bool Completed { get; set; } = false;
