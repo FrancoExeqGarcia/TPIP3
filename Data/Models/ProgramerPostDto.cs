5dcdb9c520ee76594e5a7fdb4c78df7b8ae8f0c8
Ramiro Di Carlo <ramirodicarlo2@gmail.com>
2023-12-01 11:21:31 -0300

    .

--- Data/Models/ProgramerPostDto.cs
+++ Data/Models/ProgramerPostDto.cs
@@ -4,10 +4,11 @@ namespace TODOLIST.Data.Models
 {
     public class ProgramerPostDto
     {
-
-            public string Password { get; set; }
-            public string Email { get; set; }
-            [Required]
-            public string UserName { get; set; }
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
     }
 }
